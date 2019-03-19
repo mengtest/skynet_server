017ec5c856eeaa48ec46eec60e1024865bd10134
dingdalong <330373763@qq.com>
2019-03-19 19:36:06 +0800

    test

--- common/gamedata/map.lua
+++ common/gamedata/map.lua
@@ -38,6 +38,366 @@ local map = {
     {
         name = "ten",
         maxtempid = 65535
+    },
+    {
+        name = "main1",
+        maxtempid = 65535
+    },
+    {
+        name = "second1",
+        maxtempid = 65535
+    },
+    {
+        name = "three1",
+        maxtempid = 65535
+    },
+    {
+        name = "four1",
+        maxtempid = 65535
+    },
+    {
+        name = "five1",
+        maxtempid = 65535
+    },
+    {
+        name = "six1",
+        maxtempid = 65535
+    },
+    {
+        name = "seven1",
+        maxtempid = 65535
+    },
+    {
+        name = "eight1",
+        maxtempid = 65535
+    },
+    {
+        name = "nine1",
+        maxtempid = 65535
+    },
+    {
+        name = "ten1",
+        maxtempid = 65535
+    },
+    {
+        name = "main2",
+        maxtempid = 65535
+    },
+    {
+        name = "second2",
+        maxtempid = 65535
+    },
+    {
+        name = "three2",
+        maxtempid = 65535
+    },
+    {
+        name = "four2",
+        maxtempid = 65535
+    },
+    {
+        name = "five2",
+        maxtempid = 65535
+    },
+    {
+        name = "six2",
+        maxtempid = 65535
+    },
+    {
+        name = "seven2",
+        maxtempid = 65535
+    },
+    {
+        name = "eight2",
+        maxtempid = 65535
+    },
+    {
+        name = "nine2",
+        maxtempid = 65535
+    },
+    {
+        name = "ten2",
+        maxtempid = 65535
+    },
+    {
+        name = "main3",
+        maxtempid = 65535
+    },
+    {
+        name = "second3",
+        maxtempid = 65535
+    },
+    {
+        name = "three3",
+        maxtempid = 65535
+    },
+    {
+        name = "four3",
+        maxtempid = 65535
+    },
+    {
+        name = "five3",
+        maxtempid = 65535
+    },
+    {
+        name = "six3",
+        maxtempid = 65535
+    },
+    {
+        name = "seven3",
+        maxtempid = 65535
+    },
+    {
+        name = "eight3",
+        maxtempid = 65535
+    },
+    {
+        name = "nine3",
+        maxtempid = 65535
+    },
+    {
+        name = "ten3",
+        maxtempid = 65535
+    },
+    {
+        name = "main4",
+        maxtempid = 65535
+    },
+    {
+        name = "second4",
+        maxtempid = 65535
+    },
+    {
+        name = "three4",
+        maxtempid = 65535
+    },
+    {
+        name = "four4",
+        maxtempid = 65535
+    },
+    {
+        name = "five4",
+        maxtempid = 65535
+    },
+    {
+        name = "six4",
+        maxtempid = 65535
+    },
+    {
+        name = "seven4",
+        maxtempid = 65535
+    },
+    {
+        name = "eight4",
+        maxtempid = 65535
+    },
+    {
+        name = "nine4",
+        maxtempid = 65535
+    },
+    {
+        name = "ten4",
+        maxtempid = 65535
+    },
+    {
+        name = "main5",
+        maxtempid = 65535
+    },
+    {
+        name = "second5",
+        maxtempid = 65535
+    },
+    {
+        name = "three5",
+        maxtempid = 65535
+    },
+    {
+        name = "four5",
+        maxtempid = 65535
+    },
+    {
+        name = "five5",
+        maxtempid = 65535
+    },
+    {
+        name = "six5",
+        maxtempid = 65535
+    },
+    {
+        name = "seven5",
+        maxtempid = 65535
+    },
+    {
+        name = "eight5",
+        maxtempid = 65535
+    },
+    {
+        name = "nine5",
+        maxtempid = 65535
+    },
+    {
+        name = "ten5",
+        maxtempid = 65535
+    },
+    {
+        name = "main6",
+        maxtempid = 65535
+    },
+    {
+        name = "second6",
+        maxtempid = 65535
+    },
+    {
+        name = "three6",
+        maxtempid = 65535
+    },
+    {
+        name = "four6",
+        maxtempid = 65535
+    },
+    {
+        name = "five6",
+        maxtempid = 65535
+    },
+    {
+        name = "six6",
+        maxtempid = 65535
+    },
+    {
+        name = "seven6",
+        maxtempid = 65535
+    },
+    {
+        name = "eight6",
+        maxtempid = 65535
+    },
+    {
+        name = "nine6",
+        maxtempid = 65535
+    },
+    {
+        name = "ten6",
+        maxtempid = 65535
+    },
+    {
+        name = "main7",
+        maxtempid = 65535
+    },
+    {
+        name = "second7",
+        maxtempid = 65535
+    },
+    {
+        name = "three7",
+        maxtempid = 65535
+    },
+    {
+        name = "four7",
+        maxtempid = 65535
+    },
+    {
+        name = "five7",
+        maxtempid = 65535
+    },
+    {
+        name = "six7",
+        maxtempid = 65535
+    },
+    {
+        name = "seven7",
+        maxtempid = 65535
+    },
+    {
+        name = "eight7",
+        maxtempid = 65535
+    },
+    {
+        name = "nine7",
+        maxtempid = 65535
+    },
+    {
+        name = "ten7",
+        maxtempid = 65535
+    },
+    {
+        name = "main8",
+        maxtempid = 65535
+    },
+    {
+        name = "second8",
+        maxtempid = 65535
+    },
+    {
+        name = "three8",
+        maxtempid = 65535
+    },
+    {
+        name = "four8",
+        maxtempid = 65535
+    },
+    {
+        name = "five8",
+        maxtempid = 65535
+    },
+    {
+        name = "six8",
+        maxtempid = 65535
+    },
+    {
+        name = "seven8",
+        maxtempid = 65535
+    },
+    {
+        name = "eight8",
+        maxtempid = 65535
+    },
+    {
+        name = "nine8",
+        maxtempid = 65535
+    },
+    {
+        name = "ten8",
+        maxtempid = 65535
+    },
+    {
+        name = "main9",
+        maxtempid = 65535
+    },
+    {
+        name = "second9",
+        maxtempid = 65535
+    },
+    {
+        name = "three9",
+        maxtempid = 65535
+    },
+    {
+        name = "four9",
+        maxtempid = 65535
+    },
+    {
+        name = "five9",
+        maxtempid = 65535
+    },
+    {
+        name = "six9",
+        maxtempid = 65535
+    },
+    {
+        name = "seven9",
+        maxtempid = 65535
+    },
+    {
+        name = "eight9",
+        maxtempid = 65535
+    },
+    {
+        name = "nine9",
+        maxtempid = 65535
+    },
+    {
+        name = "ten9",
+        maxtempid = 65535
     }
 }
 
