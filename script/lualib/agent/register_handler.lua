3f6984d55cc733b08163d333118257157b249d52
dingdalong <330373763@qq.com>
2020-11-15 17:06:32 +0800

    去掉无用的handler

--- script/lualib/agent/register_handler.lua
+++ script/lualib/agent/register_handler.lua
@@ -1,17 +1,11 @@
 local test_handler = require "agent.test_handler"
 local role_handler = require "agent.role_handler"
-local map_handler = require "agent.map_handler"
-local aoi_handler = require "agent.aoi_handler"
-local move_handler = require "agent.move_handler"
 
 local hander = {}
 
 function hander:register(REQUEST, CMD)
     test_handler:register(REQUEST, CMD)
     role_handler:register(REQUEST, CMD)
-    map_handler:register(REQUEST, CMD)
-    aoi_handler:register(REQUEST, CMD)
-    move_handler:register(REQUEST, CMD)
 end
 
 return hander
